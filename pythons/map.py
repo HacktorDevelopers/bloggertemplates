1f620900a89b3389eda5a52772f36b4462fe7847
toykam <mwork063@gmail.com>
2018-11-05 00:28:12 +0100

    I have learnt map function

---
+++ pythons/map.py
@@ -0,0 +1,22 @@
+#!  PYTHON PROGRAM
+
+#   LISTS
+
+print("A Program that upgrade student scores")
+
+iscore = [22, 10, 16, 30, 10, 25, 35, 20, 4, 11]
+tscore = 40
+print("Initial Score")
+
+num = 1
+
+for i in iscore:
+    perc = int((i/tscore)*100)
+    if perc < 50:
+        print(str(num)+" --- Failed with "+ str(perc)+"%")
+    else:
+        print(str(num)+" --- Passed with "+ str(perc)+"%")
+    num += 1
+    
+    #if ((i/tscore)+100) < 70:
+    #print("This Faied"
